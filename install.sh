681fd35db5ade70457b4bbc75522e523faf4f976
Jake Meyer <jakewmeyer@gmail.com>
2017-06-28 07:01:18 -0500

    updated install.sh for new version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Optional Install.sh for easy install
-curl -LO https://github.com/jakewmeyer/Geo/archive/v0.1.3.tar.gz
+curl -LO https://github.com/jakewmeyer/Geo/archive/v0.1.4.tar.gz
 
 tar -zxvf v0.1.3.tar.gz

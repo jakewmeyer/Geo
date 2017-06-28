c0a9dcde1f9905be65352d65024abbbb049c42f0
Jake Meyer <jakewmeyer@gmail.com>
2017-06-28 17:21:41 -0500

    bump version to 0.1.7 for ip addr changes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Optional Install.sh for easy install
-curl -LO https://github.com/jakewmeyer/Geo/archive/v0.1.6.tar.gz
+curl -LO https://github.com/jakewmeyer/Geo/archive/v0.1.7.tar.gz
 
-tar -zxvf v0.1.6.tar.gz
+tar -zxvf v0.1.7.tar.gz
